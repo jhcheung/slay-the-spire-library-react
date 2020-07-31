6734c28352ca2efa01642e8887d99f740b2bca3c
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2020-07-30 19:39:46 -0700

    revert changes around react-router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import './App.css';
 import MainContainer from './MainContainer.js';
-import CardsContainer from './CardsContainer.js';
-import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <h3 id="title">Slay The Spire Card Library</h3>
-          <MainContainer />
-          <div>
-            <Route path="/cards/:id" component={CardsContainer} />
-          </div>
-        </header>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <h3 id="title">Slay The Spire Card Library</h3>
+        <MainContainer />
+      </header>
+    </div>
   );
 }
 
